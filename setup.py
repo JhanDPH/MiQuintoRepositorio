9b604358cbe409c905381fa647075aa7e6e6fc95
Jhan Daniel Parra <jhandanielparrah.06@hotmail.es>
2026-02-23 10:47:22 -0500

    primer lanzamiento

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="quintorepo",                                      # Nombre del paquete
+    version="0.1.0",                                        # Versión inicial
+    packages=find_packages(),                               # Paquetes a incluir
+    description="Un paquete pip simple de saludo",          # Breve descripción
+    author="Jhan Daniel Parra",                             # Tu nombre
+    author_email="jhandanielparrah.06@hotmail.es",          # Tu correo electrónico
+    url="https://github.com/JhanDPH/MiQuintoRepositorio",   # URL del proyecto
+)
